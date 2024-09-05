2810e3fded1aa2b46d799ad1fcad7d9b0cfea58f
ChristiannYa <christian.ylopez02@gmail.com>
2024-09-05 16:50:39 -0500

    changed code to make it easier to understand, and added trail on the grid when hovered over

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,7 +1,23 @@
 const gridContainer = document.getElementById('grid-container');
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridContainer.appendChild(gridItem);
+const gridSize = 16;
+const totalCells = gridSize * gridSize;
+
+for (let i = 0; i < totalCells; i++) {
+    const gridCell = document.createElement('div');
+
+    gridCell.classList.add('grid-item');
+
+    gridContainer.appendChild(gridCell);
 }
+
+let hoveredCells = [];
+
+gridContainer.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('grid-item')) {
+
+        event.target.classList.add('hovered');
+
+        hoveredCells.push(event.target);
+    }
+});
