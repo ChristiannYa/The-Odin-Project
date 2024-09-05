26eed351aa2bcb1a4fd6c01a5cec0cef3e428109
ChristiannYa <christian.ylopez02@gmail.com>
2024-09-05 14:20:44 -0500

    added 16x16 grid

---
+++ Etch-a-Sketch/script.js
@@ -0,0 +1,7 @@
+const gridContainer = document.getElementById('grid-container');
+
+for (let i = 0; i < 16 * 16; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+}
